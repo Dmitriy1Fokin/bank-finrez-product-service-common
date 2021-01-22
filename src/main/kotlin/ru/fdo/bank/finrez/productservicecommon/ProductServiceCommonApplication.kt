c3006b3319cfc55ce3093a1847ccabf8e9762893
Dmitrii Fokin <fokinds1@gmail.com>
2021-01-22 16:48:48 +0300

    [BF-74] first commit. Init empty project.

---
+++ src/main/kotlin/ru/fdo/bank/finrez/productservicecommon/ProductServiceCommonApplication.kt
@@ -0,0 +1,11 @@
+package ru.fdo.bank.finrez.productservicecommon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProductServiceCommonApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProductServiceCommonApplication>(*args)
+}
